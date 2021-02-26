5d28987dd8773143e96a5de31e993e3edc449861
Alexander <jhojanvelasquez@seven4n.com>
2021-02-26 18:56:43 -0500

    Cambios

--- src/main/scala/example/List.scala
+++ src/main/scala/example/List.scala
@@ -9,6 +9,12 @@ case class Const[+A](h:A,t:List[A]) extends List[A]
 object List extends App {
 
   // A* seq [A]
+  def const[A](h:A, t:List[A]):List[A] = Const(h,t)
+
+  def apply[A](as: A*) : List[A] ={
+    if (as.isEmpty) Nil
+    else Const(as.head, apply(as.tail: _*))
+  }
 
   /*
   Tema 2 Construcción de listas
@@ -20,144 +26,26 @@ object List extends App {
   existen de la lista
    */
 
-  def length[A](lst:List[A]):Int= lst match {
-    case Nil => 0
-    case Const(h,t) => 1 + length(t)
-  }
-  def sum(ints: List[Int]):Int = ints match {
-    case Nil => 0
-    case Const(h,t) => h + sum(t)
-  }
-  def product(ds: List[Double]):Double = ds match {
-    case Nil => 1
-    case Const(h,t) => h * product(t)
-  }
-  def apply[A](as: A*) : List[A] ={
-    if (as.isEmpty) Nil
-    else Const(as.head, apply(as.tail: _*))
-  }
-  /*
-   Tema 2 Coincidencia de patrones
-*/
-  /*
-  Ejercicio 1 ¿Cuál es el resultado de la siguiente expresión match?
-   */
-  val x = List(4,5,6,7,8) match {
-    case Const(x, Const(5, Const(7, _))) => x
-    case Nil => 1
-    case Const(x, Const(y, Const(6, Const(7, _)))) => x + y
-    case Const(h, t) => h + sum(t)
-    case _ => 777
-  }
-  //El valor que tiene x es 9
-
-  /*
-  Ejercicio 2. Implementa la función tail que remueva el primer elemento de un lista
-   */
-  def tail[A](lst:List[A]):List[A]= lst match {
-    case Nil => Nil
-    case Const(h,t) => t
-  }
-
-  /*
-  Ejercicio 3. Implementa la función head que devuelva el primer elemento de la lista.
-   */
-  def head[A](lst:List[A]):A = lst match {
-    case Const(h,t) => h
-  }
-  /*
-    Ejercicio 4. Implemente la siguiente función. def and(lst:List[Boolean]):Boolean = ? ? ?
-   */
-  def and(lst:List[Boolean]):Boolean = lst match {
-    case Nil => true
-    case Const(h,Nil) => h
-    case Const(h,t) => h && and(t)
-  }
-  /*
-  Ejercicio 5. Implemente la siguiente función. def or(lst:List[Boolean]):Boolean = ???
-   */
-  def or(lst:List[Boolean]):Boolean = lst match {
-    case Nil => false
-    case Const(h,Nil) => h
-    case Const(h,t) => h || or(t)
-  }
-  /*
-  Ejercicio 6. Implemente la siguiente función.
-   */
-  def max(lst:List[Int]):Int = {
-    @tailrec
-    def maxr(lst:List[Int], max:Int):Int = lst match {
-      case Nil => max
-      case Const (h, t) => maxr (t, if (h > max) h else max)
-    }
-    maxr(tail(lst),head(lst))
-  }
-  /*
-  Ejercicio 7. Implemente la siguiente función.
-   */
-  def min(lst:List[Int]):Int = {
-    @tailrec
-    def minr(lst:List[Int], min:Int):Int = lst match {
-      case Nil => min
-      case Const (h, t) => minr (t, if (h < min) h else min)
-    }
-    minr(tail(lst),head(lst))
-  }
-  /*
-  Ejercicio 8. Implemente la siguiente función. def minMax(lst:List[Double]):(Double,Double) = ? ? ?
-   */
-  def minMax(lst:List[Double]):(Double,Double) = {
-    def selectionmm(op:Int, n1:Double,n2:Double):Double = op match {
-      case 1 => if(n1 > n2) n1 else n2
-      case _ => if(n1 < n2) n1 else n2
-    }
-    @tailrec
-    def minMaxR(lst:List[Double],mm:(Double,Double)):(Double,Double) = lst match {
-      case Nil => mm
-      case Const(h,t) => minMaxR(t, (selectionmm(0,h,mm._1),selectionmm(1,h,mm._2)))
-    }
-    minMaxR(tail(lst),(head(lst),head(lst)))
-  }
-  val l1 = List(1,2,3,4,5,6,7,8)
-  val ll = List(4L,5L,6L,7L,8L)
-  val lmm = List(4.7,5.1,6.2,7.4,8.7)
-  //println(l1)
-  //println(tail(List(4)))
-  //println(head(l1))
-  val lb = List(true,false)
-  //println(and(lb))
-  //println(or(lb))
-  //println(max(l1))
-  //println(min(ll))
-  //println(minMax(lmm))
-  //println("el valor de x es : " + x)
-
-  def const[A](h:A, t:List[A]):List[A] = Const(h,t)
-
-  def addEnd[A](h:A, t:List[A]):List[A] = t match {
-    case Const(h1,t2) => Const(h1,addEnd(h,t2))
-    case Nil => Const(h,Nil)
-  }
-
-  def append[A](lst1:List[A],lst2:List[A]):List[A] = (lst1, lst2) match {
-    case (Nil,Nil) => Nil
-    case (l1,Nil) => l1
-    case (Nil,l2) => l2
-    case (Const(h,t),l2) => Const(h,append(t,l2))
-  }
-
-  def appendOrdAsc(lst1:List[Int],lst2:List[Int]):List[Int] = (lst1, lst2) match {
-    case (Nil,Nil) => Nil
-    case (l1,Nil) => l1
-    case (Nil,l2) => l2
-    case (Const(h,t),Const(h2,t2)) => if (h<h2) Const(h,appendOrdAsc(t,Const(h2,t2))) else Const(h2,appendOrdAsc(Const(h,t),t2))
-  }
-
-  def drop[A](pos:Int,lst:List[A]):List[A] = (pos,lst) match {
+  def take[A](n:Int,lst:List[A]):List[A] = (n,lst) match {
+    case (0,_) => Nil
     case (n,Nil) => Nil
-    case (0,l) => l
-    case (n,Const(h,t)) => drop(n-1,t)
+    case (n,Const(h,t)) => Const(h,take(n-1,t))
   }
+
+  /*
+  Ejercicio 2. Implemente la función init que tiene toma una lista y toma los valores
+  iniciales excepto el último.
+   */
+
+  def init[A](lst:List[A]):List[A] = lst match {
+    case Const(h,Nil) => Nil
+    case Const(h,t) => Const(h,init(t))
+  }
+
+  /*
+  Ejercicio 3. Implemente la función split, recibe dos parámetros n y una lista;
+divide la primera lista en n elementos y los restantes quedan en la segunda lista.
+   */
 
   def split[A](pos:Int,lst:List[A]):(List[A],List[A]) = {
     @tailrec
@@ -168,13 +56,71 @@ object List extends App {
     }
     splitAux(pos,lst,Nil)
   }
-  println(l1)
-  println(split(2,l1))
-  println(split(4,l1))
-  println(split(0,l1))
-  println(split(8,l1))
-  println(split(10,l1))
-  //println(append(l1,lb))
-  //println(appendOrdAsc(Const(3,Const(5,Const(6,Nil))), Const(1,Const(2,Const(4,Const(7,Nil))))))
+
+  /*
+  Ejercicio 4. Implemente la función zip esta función fusiona dos listas de tipos diferentes
+  en una lista de pares del mismo tamaño. La siguiente es la firma de la función:
+   */
+
+  def zip[A,B](lst1:List[A],lst2:List[B]):List[(A,B)] = (lst1,lst2) match {
+    case (Const(h,t),Nil) => Nil
+    case (Nil,Const(h,t)) => Nil
+    case (Const(h,t),Const(h2,t2)) => Const((h,h2),zip(t,t2))
+  }
+
+  /*
+  Ejercicio 5. Implemente la función unzip esta lista separa una lista de tuplas
+  en dos listas distintas.
+   */
+
+  def addEnd[A](h:A, t:List[A]):List[A] = t match {
+    case Const(h1,t2) => Const(h1,addEnd(h,t2))
+    case Nil => Const(h,Nil)
+  }
+
+  def unzip[A,B](lst:List[(A,B)]):(List[A],List[B]) = {
+    @tailrec
+    def unzipRec[A,B](lst:List[(A,B)],l1:List[A],l2:List[B]):(List[A],List[B]) = lst match {
+      case Nil => (l1,l2)
+      case Const(h,t) => unzipRec(t,addEnd(h._1,l1),addEnd(h._2,l2))
+    }
+    unzipRec(lst,Nil,Nil)
+  }
+
+  /*
+  Ejercicio 6. Implemente la función reverse. Toma una lista y devuelve una
+  versión invertida de la misma.
+   */
+  def reverse[A](lst:List[A]):List[A] = lst match {
+    case Nil => Nil
+    case Const(h,t) => addEnd(h,reverse(t))
+  }
+  //println(reverse(Const(3,Const(2,Const(1,Nil)))))
+  /*
+  Ejercicio 7. Implemente la función intersperse. Esta se encarga de entremezclar
+  un valor entre los elementos originales de la lista.
+   */
+  def intersperse[A](elem:A,lst:List[A]):List[A] = lst match {
+    case Nil => Nil
+    case Const(h,Nil) => Const(h,Nil)
+    case Const(h,t) => Const(h,Const(elem,intersperse(elem,t)))
+  }
+
+  /*
+  Ejercicio 8. Implemente la función concat. Es función recibe una lista de lista
+  valores de un tipo A y la transforma en una lista de valores de tipo A.
+   */
+
+  def append[A](lst1:List[A],lst2:List[A]):List[A] = (lst1, lst2) match {
+    case (Nil,Nil) => Nil
+    case (l1,Nil) => l1
+    case (Nil,l2) => l2
+    case (Const(h,t),l2) => Const(h,append(t,l2))
+  }
+
+  def concat[A](lst:List[List[A]]):List[A] = lst match {
+    case Nil => Nil
+    case Const(h,t) => append(h,concat(t))
+  }
 
 }
--- src/main/scala/example/Nat.scala
+++ src/main/scala/example/Nat.scala
@@ -2,130 +2,58 @@ package example
 
 import scala.annotation.tailrec
 
-sealed trait List [+A]
-case object Nil extends List[Nothing]
-case class Const[+A](h:A,t:List[A]) extends List[A]
+sealed trait Nat
+case object Cero extends Nat
+case class Suc(nat:Nat) extends Nat
 
-object List extends App {
+object Nat extends App {
 
-  // A* seq [A]
-  def const[A](h:A, t:List[A]):List[A] = Const(h,t)
+  /*
+  Tema 3 Naturales
+   */
+  /*
+  Ejercicio 9. Implemente la función addNat. Esta función recibe dos naturales y
+  se encarga de sumarlos produciendo un valor correcto
+   */
 
-  def apply[A](as: A*) : List[A] ={
-    if (as.isEmpty) Nil
-    else Const(as.head, apply(as.tail: _*))
+  def addNat(nat1:Nat,nat2:Nat):Nat = nat1 match {
+    case Cero => nat2
+    case Suc(nat) => addNat(nat,Suc(nat2))
   }
-
   /*
-  Tema 2 Construcción de listas
-   */
+  println(addNat(Cero,Suc(Cero)))
+  println(addNat(Suc(Suc(Cero)),Suc(Cero)))
+  println(addNat(Suc(Suc(Cero)),Suc(Suc(Cero))))
+*/
   /*
-  Ejercicio 1 Implemente la función take que se encarga de tomar dos parámetros.
-  El primero un valor entero positivo n y el segundo una lista de valores de
-  cualquier tiempo. Y esta función se encarga de tomar los n primeros valores, si
-  existen de la lista
-   */
-
-  def take[A](n:Int,lst:List[A]):List[A] = (n,lst) match {
-    case (0,_) => Nil
-    case (n,Nil) => Nil
-    case (n,Const(h,t)) => Const(h,take(n-1,t))
-  }
-
-  /*
-  Ejercicio 2. Implemente la función init que tiene toma una lista y toma los valores
-  iniciales excepto el último.
+    Ejercicio 10. Implemente la función prodNat. Esta función realiza la multipli-
+  cación de dos valores naturales.
    */
 
-  def init[A](lst:List[A]):List[A] = lst match {
-    case Const(h,Nil) => Nil
-    case Const(h,t) => Const(h,init(t))
-  }
-
-  /*
-  Ejercicio 3. Implemente la función split, recibe dos parámetros n y una lista;
-divide la primera lista en n elementos y los restantes quedan en la segunda lista.
-   */
-
-  def split[A](pos:Int,lst:List[A]):(List[A],List[A]) = {
-    @tailrec
-    def splitAux[A](pos:Int,lst:List[A],lst2:List[A]):(List[A],List[A]) = (pos,lst) match {
-      case (a,Nil) => (lst2,Nil)
-      case (0,lst) => (lst2,lst)
-      case (a,Const(h,t)) => splitAux(a-1,t,addEnd(h,lst2))
+  def prodNat(nat1:Nat,nat2:Nat):Nat = {
+    if (nat1 == Cero || nat2 == Cero) {
+      return Cero
     }
-    splitAux(pos,lst,Nil)
-  }
-
-  /*
-  Ejercicio 4. Implemente la función zip esta función fusiona dos listas de tipos diferentes
-  en una lista de pares del mismo tamaño. La siguiente es la firma de la función:
-   */
-
-  def zip[A,B](lst1:List[A],lst2:List[B]):List[(A,B)] = (lst1,lst2) match {
-    case (Const(h,t),Nil) => Nil
-    case (Nil,Const(h,t)) => Nil
-    case (Const(h,t),Const(h2,t2)) => Const((h,h2),zip(t,t2))
-  }
-
-  /*
-  Ejercicio 5. Implemente la función unzip esta lista separa una lista de tuplas
-  en dos listas distintas.
-   */
-
-  def addEnd[A](h:A, t:List[A]):List[A] = t match {
-    case Const(h1,t2) => Const(h1,addEnd(h,t2))
-    case Nil => Const(h,Nil)
-  }
-
-  def unzip[A,B](lst:List[(A,B)]):(List[A],List[B]) = {
-    @tailrec
-    def unzipRec[A,B](lst:List[(A,B)],l1:List[A],l2:List[B]):(List[A],List[B]) = lst match {
-      case Nil => (l1,l2)
-      case Const(h,t) => unzipRec(t,addEnd(h._1,l1),addEnd(h._2,l2))
+    else if(nat1 == Suc(Cero)){
+      return nat2
     }
-    unzipRec(lst,Nil,Nil)
+    else if(nat2 == Suc(Cero)){
+      return nat1
+    }
+    else{
+      @tailrec
+      def prodNatR(nat1:Nat,nat2:Nat, result:Nat):Nat = nat1 match {
+        case Cero => result
+        case Suc(n) => prodNatR(n,nat2,addNat(result,nat2))
+      }
+      prodNatR(nat1,nat2,Cero)
+    }
   }
-
   /*
-  Ejercicio 6. Implemente la función reverse. Toma una lista y devuelve una
-  versión invertida de la misma.
-   */
-  def reverse[A](lst:List[A]):List[A] = lst match {
-    case Nil => Nil
-    case Const(h,t) => addEnd(h,reverse(t))
-  }
-  //println(reverse(Const(3,Const(2,Const(1,Nil)))))
-  /*
-  Ejercicio 7. Implemente la función intersperse. Esta se encarga de entremezclar
-  un valor entre los elementos originales de la lista.
-   */
-  def intersperse[A](elem:A,lst:List[A]):List[A] = lst match {
-    case Nil => Nil
-    case Const(h,Nil) => Const(h,Nil)
-    case Const(h,t) => Const(h,Const(elem,intersperse(elem,t)))
-  }
-
-  /*
-  Ejercicio 8. Implemente la función concat. Es función recibe una lista de lista
-  valores de un tipo A y la transforma en una lista de valores de tipo A.
-   */
-
-  def append[A](lst1:List[A],lst2:List[A]):List[A] = (lst1, lst2) match {
-    case (Nil,Nil) => Nil
-    case (l1,Nil) => l1
-    case (Nil,l2) => l2
-    case (Const(h,t),l2) => Const(h,append(t,l2))
-  }
-
-  def concat[A](lst:List[List[A]]):List[A] = lst match {
-    case Nil => Nil
-    case Const(h,t) => append(h,concat(t))
-  }
-
-  val l1 = List(1,2,3,4,5,6,7,8)
-  val ll = List(4L,5L,6L,7L,8L)
-  val lmm = List(4.7,5.1,6.2,7.4,8.7)
-  val lb = List(true,false)
+  println(prodNat(Cero,Suc(Cero)))
+  println(prodNat(Suc(Suc(Cero)),Suc(Cero)))
+  println(prodNat(Suc(Suc(Suc(Cero))),Suc(Suc(Cero))))
+  println(prodNat(Suc(Suc(Suc(Suc(Suc(Cero))))),Suc(Suc(Suc(Suc(Cero))))))
+  */
 
 }
